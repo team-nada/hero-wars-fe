abc083d2dbe168d5782182f9089cf800754c0834
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-05-13 15:16:44 -0700

    add state and method to app.js

--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,31 @@ import React from 'react';
 
 class App extends React.Component {
   
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: null,
+      view: 'home',
+      gameCards: [],
+      activePlayerCards: {},
+      activeCompCards: {},
+      userScore: 0,
+      compScore: 0,
+      win: false,
+    }
+  }
+
+  // Makes call to server
+  getCards = () => {
+    console.log('get cards');
+  }
 
   render(){
     return (
-      <p> Hello</p>
+      <p> Welcome To Hero wars</p>
     );
   }
-  
 }
 
 export default App;
