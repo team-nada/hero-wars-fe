27f0791ef867e1f08cc9ab9bfe984789a5b8542b
Roger Huba <rogerhuba@gmail.com>
2019-05-15 12:32:53 -0700

    Win page working

--- src/components/game.js
+++ src/components/game.js
@@ -4,6 +4,7 @@ import Card from './card.js';
 import PlayerHand from './playerHand.js';
 import Score from './score.js';
 import ComputerHand from './computerHand.js';
+import Win from './win.js';
 
 class Game extends React.Component {
   constructor(props){
@@ -15,13 +16,20 @@ class Game extends React.Component {
     return (
       <React.Fragment>
         <h2>{this.props.username} vs. Computer</h2>
-        <ComputerHand hand={this.props.computerCards} 
+        <ComputerHand hand={this.props.computerCards}
           activeCard={this.props.computerActiveCard}
           handleClickFunction={this.props.handleClickFunction}/>
-        <PlayerHand hand={this.props.playerCards} 
+        <PlayerHand hand={this.props.playerCards}
           activeCard={this.props.playerActiveCard}
           handleClickFunction={this.props.handleClickFunction}/>
         <Score />
+
+        {/*Need to add logic to display the Win page*/}
+
+        <Win playersScore={this.props.playersScore}
+          comuterScore={this.props.computerScore}
+          handleMainMenu={this.props.handleMainMenu}
+          handlePlayAgain={this.props.handlePlayAgain}/>
       </React.Fragment>
     );
   }
