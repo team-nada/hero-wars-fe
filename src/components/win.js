fd05e5f36b24f8500b70639e2169ba319451706b
Paula Thomas <paula9t9@gmail.com>
2019-05-16 11:08:43 -0700

    Playflow now works as expected

--- src/components/win.js
+++ src/components/win.js
@@ -10,8 +10,8 @@ class Win extends React.Component {
   render(){
     return (
       <React.Fragment>
-        <p id="final_results">Player Score: {this.props.computerScore}  Computer Score: {this.props.playersScore}</p>
-        {(this.props.computerScore > this.props.playersScore) ? <p>Computer Wins</p> : (this.props.computerScore < this.props.playersScore) ? <p>Player Wins</p> : <p>Player and Computer Tie</p>}
+        <p id="final_results">Player Score: {this.props.playerScore}  Computer Score: {this.props.computerScore}</p>
+        {(this.props.computerScore > this.props.playerScore) ? <p>Computer Wins</p> : (this.props.computerScore < this.props.playerScore) ? <p>Player Wins</p> : <p>Player and Computer Tie</p>}
         <button onClick={this.props.handlePlayAgain}>Play Again</button>
         <button onClick={this.props.handleMainMenu}>Main Menu</button>
       </React.Fragment>
