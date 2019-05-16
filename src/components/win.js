ba4fb72c7924879bceffd61b10222fe4bf394d9f
Jhia Turner <jhimitu@gmail.com>
2019-05-16 16:15:42 -0700

    style wins page

--- src/components/win.js
+++ src/components/win.js
@@ -11,13 +11,13 @@ class Win extends React.Component {
     return (
       <React.Fragment>
 
-        <div className="gameFooter">
-        <p id="final_results">Player Score: {this.props.playerScore}  Computer Score: {this.props.computerScore}</p>
-        {(this.props.computerScore > this.props.playerScore) ? <p>Computer Wins</p> : 
-          (this.props.computerScore < this.props.playerScore) ? <p>Player Wins</p> : 
-          <p>Player and Computer Tie</p>}
-        <button onClick={this.props.handlePlayAgain}>Play Again</button>
-        <button onClick={this.props.handleMainMenu}>Main Menu</button>
+        <div className="win-page">
+        <p className="final-results-1">Player Score: {this.props.playerScore}<br/>  Computer Score: {this.props.computerScore}</p>
+        {(this.props.computerScore > this.props.playerScore) ? <p className="wins">Computer Wins</p> : 
+          (this.props.computerScore < this.props.playerScore) ? <p className="wins">Player Wins</p> : 
+          <p className="final-results-2">Player and Computer Tie</p>}
+        <button className="button button-1" onClick={this.props.handlePlayAgain}>Play Again</button>
+        <button className="button button-2" onClick={this.props.handleMainMenu}>Main Menu</button>
 
         </div>
       </React.Fragment>
