b4a0e33c10ea2ecc02d48aca0f6d50b640053068
Antberry <aniej2013@gmail.com>
2019-05-16 10:49:56 -0700

    designing player card

--- src/components/card.js
+++ src/components/card.js
@@ -12,7 +12,7 @@ class Card extends React.Component {
           <h2>{this.props.card ? this.props.card.name : 'John Snow'}</h2>
           <img src={this.props.card ? this.props.card.image_url : 'https://image.cnbcfm.com/api/v1/image/102952386-1475947_10152940854093933_1311401295196984207_n.jpg?v=1503328916&w=1400&h=950'} alt="heroes img"/>
           <h3>{this.props.card ? this.props.card.power : '100'}</h3>
-          <p>The character {this.props.card ? this.props.card.name : 'John Snow'} is a part of the {this.props.card ? this.props.card.race : 'House Targaryen'} race, and is affiliated with {this.props.card ? this.props.card.groupaffiliation : 'House Stark'}.</p>
+          {/* <p>The character {this.props.card ? this.props.card.name : 'John Snow'} is a part of the {this.props.card ? this.props.card.race : 'House Targaryen'} race, and is affiliated with {this.props.card ? this.props.card.groupaffiliation : 'House Stark'}.</p> */}
         </div>
     );
   }
